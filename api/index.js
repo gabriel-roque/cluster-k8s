1b0707f25645ade7d8697e586ab7d07c9d2ac394
gabriel-roque <gabrielrms.dll@gmail.com>
2022-02-25 15:14:51 -0300

    feat: configure apm server

--- api/index.js
+++ api/index.js
@@ -1,29 +1,64 @@
-const app = require('express')();
+require('dotenv').config();
+
+const app = require('express')()
+const faker = require('faker')
 
 const apm = require('elastic-apm-node').start({
-  // Override service name from package.json
-  // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-  serviceName: 'Node_API',
-
-  // Use if APM Server requires a token
-  // secretToken: '',
-
-  // Use if APM Server uses API keys for authentication
-  apiKey: 'Um9oeEYzOEJFUmlaNm9BYzMzbHc6N3Z4TDRzWjNSUnFGRkF1d211NUlXZw==',
-
-  // Set custom APM Server URL (default: http://localhost:8200)
-  serverUrl: 'http://192.168.0.11:8200',
+  serviceName: 'NODE_API',
+  apiKey: process.env.ELASTIC_API_TOKEN,
+  serverUrl: process.env.APM_SERVER,
+  environment: 'development',
 })
 
-apm.logger.info('APM is running')
+app.use((req, res, next) => {
+  const t = apm.startTransaction(`${req.method} ${req.path}`, { startTime: Date.now().toString() })
+
+  const user = {
+    email: faker.internet.email(),
+    username: faker.internet.userName(),
+    id: faker.datatype.uuid()
+  }
+
+  req.user = user
+  t.addLabels(user, true)
+  apm.setUserContext(user)
+
+  apm.setCustomContext({
+    userId: user.id,
+    userAuth: true
+  })
+
+  apm.registerMetric('any_metric', () => {
+    return Math.random()
+  })
+
+  apm.setSpanOutcome('success')
+
+  req.transaction = t
+  req.id = faker.datatype.uuid()
+  next()
+})
 
 app.get('/error', (req, res) => {
-  apm.captureError(new Error('Something went wrong'));
-  return res.json({ message: 'Hello World' });
+  try {
+    throw new Error('Something went wrong')
+  } catch (err) {
+    req.transaction.end(req.id);
+    apm.captureError(err);
+    return res.json({ error: err.message });
+  }
 })
 
 app.get('/', (req, res) => {
+  req.transaction.end(req.id);
   return res.json({ message: 'Hello World' });
+})
+
+app.get('/delay', (req, res) => {
+  setTimeout(() => {
+    req.transaction.end(req.id);
+    return res.json({ message: 'Deplay Route' });
+  }, 3000)
 })
 
 app.listen(8080, () => console.log('API runing!'))
